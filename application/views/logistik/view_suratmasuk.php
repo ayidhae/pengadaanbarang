0d83057da7771cb11cef07f17c5d2c1d2bdee98a
gunda <gundariamborosati1998@gmail.com>
2018-08-14 21:45:28 +0700

    new
    new

--- application/views/logistik/view_suratmasuk.php
+++ application/views/logistik/view_suratmasuk.php
@@ -131,6 +131,7 @@
           <table id="dataCustomer" class="table ">
             <thead>
               <tr>
+                <TH>No</TH>
                 <th>Asal </th>
                 <th> jenis_surat </th>
                 <th> penanggung jawab </th>
@@ -144,12 +145,13 @@
             </thead>
             <tbody>
              <?php              
-
+             $i = 1; 
              foreach($surat_approve as $st):
 
               ?>
 
               <tr>
+              <td><?php echo $i; ?></td>
                <td><?php echo $st->username ;?></td> 
                <td><?php echo $st->jenis_surat ;?></td>
                <td><?php echo $st->penanggung_jawab ;?></td>
@@ -159,7 +161,7 @@
 
                <td><?php echo "<br><b>File : </b><i><a href='".base_URL()."asset/upload/surat_keluar/".$st->file."' target='_blank'>".$st->file."</a>"?></td> 
 
-                <td><?php echo $st->status_approve ;?></td> 
+                <td><?php echo $st->status_approve ;$i++;?></td> 
 
                 <td> <?php if($st->status_dipesanlogistik !== null){
                       echo ("Data pesanan telah dibuat!");
